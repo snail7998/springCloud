475f8278b58274b777430f0a399f1c247a555483
taozhi <taozhi@hd123.com>
2020-07-28 23:36:50 +0800

    Initial commit

---
+++ Eureka-Provider/src/main/java/com/snail/eurekaserver/EurekaProviderApplication.java
@@ -0,0 +1,24 @@
+/**
+ * 版权所有(C)，上海海鼎信息工程股份有限公司，2020，所有权利保留。
+ * <p>
+ * 项目名：	springCloud
+ * 文件名：	EurekaProviderApplication.java
+ * 模块说明：
+ * 修改历史：
+ * 2020/7/28 - taozhi - 创建。
+ */
+package com.snail.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author taozhi
+ */
+@SpringBootApplication
+public class EurekaProviderApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EurekaProviderApplication.class, args);
+  }
+}
